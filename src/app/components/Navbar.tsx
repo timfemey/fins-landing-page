b1ef4faf4601b89479247209caeecbd3d55cd995
timfemey <isholaobafemi@gmail.com>
2024-03-09 18:39:06 +0100

    fix: put correct link path in href

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
-              href="/getting-started"
+              href="/get-started"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Log in <span aria-hidden="true">&rarr;</span>
@@ -93,7 +93,7 @@ const Navbar = () => {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/get-started"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
